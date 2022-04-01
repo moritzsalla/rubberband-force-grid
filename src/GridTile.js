176424648bcf3706559b9b97c4d7ec4105114e80
Moritz <moritz@buildinamsterdam.com>
2022-04-01 12:28:53 +0200

    Better parallax

--- src/GridTile.js
+++ src/GridTile.js
@@ -1,10 +1,6 @@
-import {
-  motion,
-  useSpring,
-  useTransform,
-  useViewportScroll,
-} from 'framer-motion';
+import { motion, useTransform, useViewportScroll } from 'framer-motion';
 import styled from 'styled-components';
+import * as d3 from 'd3';
 
 const ImageWrapper = styled(motion.div)`
   height: 100%;
@@ -24,13 +20,17 @@ const nodeTransition = {
   duration: 0.65,
 };
 
+const random = d3.randomUniform(0, 100);
+
 const GridTile = ({ url, width, height, x, y }) => {
   const { scrollY } = useViewportScroll();
   const parallax = useTransform(
     scrollY,
     [0, window.innerHeight],
-    [0, -window.innerHeight * 2]
+    [0, -1000 - y * 0.5 + random()]
   );
+
+  console.log(x);
 
   return (
     <ImageWrapper
